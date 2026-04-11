d6ba21ccbf7df398b934dd2eb8301eab1c157ec7
hard828 <hardpatel802208@gmail.com>
2026-04-11 10:01:13 +0530

    Update P-17 Save in txt.py

--- P-17 Save in txt.py
+++ P-17 Save in txt.py
@@ -1,17 +1,15 @@
 # Write a program to read user name of students from console and save in a fitle(.txt).
 
-# Ask user for number of students
-n = int(input("Enter number of students: "))
+n = int(input("Enter number of students =  "))
 
-# Open file in write mode
 file = open("students.txt", "w")
 
-# Read names and write to file
 for i in range(n):
     name = input("Enter student name: ")
     file.write(name + "\n")
 
-# Close the file
-file.close()
+file = open("students.txt","r")
 
-print("Student names saved successfully in students.txt")
+print(file.read())
+
+file.close()
