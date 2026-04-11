ac6860893f511f6ae695c13040a751439551893d
hard828 <hardpatel802208@gmail.com>
2026-04-11 09:56:04 +0530

    Update P-12 Fibonacci.py

--- P-12 Fibonacci.py
+++ P-12 Fibonacci.py
@@ -1,17 +1,11 @@
 # Write a function to generate N fibonacci numbers. Make sure to ask the user to enter the value of N.
 
-# Function to generate N Fibonacci numbers
-def fibonacci(n):
-    a = 0
-    b = 1
+n = int(input("Enter the value of N = "))
+a = 0
+b = 1
 
-    print("Fibonacci Series:")
-    for i in range(n):
-        print(a, end=" ")
-        a, b = b, a + b
-
-# Ask user for value of N
-n = int(input("Enter the value of N: "))
-
-# Call the function
-fibonacci(n)
+for i in range(n):
+    print(a, end=" ")
+    c=a+b
+    a=b
+    b=c
