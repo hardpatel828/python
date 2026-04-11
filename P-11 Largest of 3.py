0217ecde218c895423b76f1eb876433484a2d9af
hard828 <hardpatel802208@gmail.com>
2026-04-11 09:55:02 +0530

    Update P-11 Largest of 3.py

--- P-11 Largest of 3.py
+++ P-11 Largest of 3.py
@@ -1,19 +1,12 @@
 # Implement a function that takes as input three variables, and returns the largest of the three.
 
-# Function to find largest of three numbers
-def largest_of_three(a, b, c):
-    if a >= b and a >= c:
-        return a
-    elif b >= a and b >= c:
-        return b
-    else:
-        return c
+a = int(input("Enter first number  = "))
+b = int(input("Enter second number = "))
+c = int(input("Enter third number  = "))
 
-# Take input from user
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-z = int(input("Enter third number: "))
-
-# Call function and print result
-largest = largest_of_three(x, y, z)
-print("Largest number is:", largest)
+if a >= b and a >= c:
+    print("Largest number is = ",a)
+elif b >= a and b >= c:
+    print("Largest number is = ",b)
+else:
+    print("Largest number is = ",c)
