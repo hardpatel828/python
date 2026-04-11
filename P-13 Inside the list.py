385f0354854ba06522fb5ea9b8e07f37e0eb6272
hard828 <hardpatel802208@gmail.com>
2026-04-11 09:56:49 +0530

    Update P-13 Inside the list.py

--- P-13 Inside the list.py
+++ P-13 Inside the list.py
@@ -1,32 +1,30 @@
 # Write a function that takes an ordered list of numbers and another number. The function decides
 # whether or not the given number is inside the list using binary search.
 
-# Function to perform binary search
-def binary_search(arr, target):
+def binary_search(list,item):
     low = 0
-    high = len(arr) - 1
+    high = len(list)-1
 
     while low <= high:
-        mid = (low + high) // 2
+        mid = (low + high)//2
+        mid_value = list[mid]
 
-        if arr[mid] == target:
-            return True
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
+        if mid_value < item :
+            low = mid + 1 
+                              
+        elif mid_value > item :
             high = mid - 1
+
+        else:
+            return True
 
     return False
 
+a = list(map(int ,input("Enter sorted numbers: ").split()))
+b = int(input("Enter number to search: "))
 
-# Take input from user
-numbers = list(map(int, input("Enter sorted numbers: ").split()))
-search = int(input("Enter number to search: "))
+found = binary_search(a,b)
 
-# Call function
-found = binary_search(numbers, search)
-
-# Print result
 if found:
     print("Number is present in the list.")
 else:
