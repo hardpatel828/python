281d77bbf8b2dc8deafbbcd352ae9cae403821e3
hard828 <hardpatel802208@gmail.com>
2026-04-25 23:49:33 +0530

    Update and rename P-22 Division and Handles.py to P-22 Exception.py

--- P-22 Exception.py
+++ P-22 Exception.py
@@ -1,19 +1,16 @@
 # Write a Python program that executes division and handles an ArithmeticError exception if there is an
 # arithmetic error.
 
-# Program to perform division with exception handling
-
 try:
-    # Take input from user
+    
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
 
-    # Perform division
     result = a / b
     print("Result of division:", result)
 
 except ArithmeticError:
-    # Handles arithmetic errors like division by zero
+   
     print("Arithmetic Error occurred! Division by zero is not allowed.")
 
 else:
