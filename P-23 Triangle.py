489c647dace208d134eb741b748192575a350fe4
hard828 <hardpatel802208@gmail.com>
2026-04-25 23:49:56 +0530

    Update P-23 Triangle.py

--- P-23 Triangle.py
+++ P-23 Triangle.py
@@ -1,15 +1,11 @@
 # Write a Python program to print a triangle using Turtle.
 
-# Import turtle module
 import turtle
 
-# Create turtle object
 t = turtle.Turtle()
 
-# Draw triangle
 for i in range(3):
-    t.forward(100)   # move forward
-    t.left(120)      # turn left
+    t.forward(100)   
+    t.right(120)     
 
-# Keep the window open
 turtle.done()
